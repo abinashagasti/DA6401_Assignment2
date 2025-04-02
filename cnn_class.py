4c372d7ba77913aeec5f524ea421b700b0431581
abinashagasti <abinashagastiof99@gmail.com>
2025-04-02 21:55:17 +0530

    working cnn implementation

--- cnn_class.py
+++ cnn_class.py
@@ -6,32 +6,38 @@ class CNN(nn.Module):
     def __init__(self, num_filters=[32, 64, 128, 256, 512], kernel_size=5, num_dense=256, activation=F.relu):
         super(CNN, self).__init__()
         self.activation = activation  # Set activation function
-        
-        # Define convolutional layers
-        self.conv1 = nn.Conv2d(3, num_filters[0], kernel_size, padding=0)
-        self.conv2 = nn.Conv2d(num_filters[0], num_filters[1], kernel_size, padding=0)
-        self.conv3 = nn.Conv2d(num_filters[1], num_filters[2], kernel_size, padding=0)
-        self.conv4 = nn.Conv2d(num_filters[2], num_filters[3], kernel_size, padding=0)
-        self.conv5 = nn.Conv2d(num_filters[3], num_filters[4], kernel_size, padding=0)
 
         # Define max-pooling layer
         self.pool = nn.MaxPool2d(2, 2)  # 2x2 pooling
+        
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, num_filters[0], kernel_size),
+            self.activation(),
+            self.pool,
+            nn.Conv2d(num_filters[0], num_filters[1], kernel_size),
+            self.activation(),
+            self.pool,
+            nn.Conv2d(num_filters[1], num_filters[2], kernel_size),
+            self.activation(),
+            self.pool,
+            nn.Conv2d(num_filters[2], num_filters[3], kernel_size),
+            self.activation(),
+            self.pool,
+            nn.Conv2d(num_filters[3], num_filters[4], kernel_size),
+            self.activation(),
+            self.pool
+        )
+        
 
-        # Compute final feature map size after 5 conv + pooling layers
-        final_size = 224 // (2**5)  # Divide by 2 for each maxpool layer
+        self.fc_layers = nn.Sequential(
+            nn.Linear(flattened_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, num_classes)
+        )
 
-        # Fully connected layers
-        self.fc1 = nn.Linear(num_filters[4] * final_size * final_size, num_dense)
-        self.fc2 = nn.Linear(num_dense, 10)  # Output layer (10 classes)
 
     def forward(self, x):
-        x = self.pool(self.activation(self.conv1(x)))
-        x = self.pool(self.activation(self.conv2(x)))
-        x = self.pool(self.activation(self.conv3(x)))
-        x = self.pool(self.activation(self.conv4(x)))
-        x = self.pool(self.activation(self.conv5(x)))
-        
-        x = torch.flatten(x, 1)  # Flatten before FC layer
-        x = self.activation(self.fc1(x))
-        x = self.fc2(x)  # No activation here (will use CrossEntropyLoss)
+        x = self.conv_layers(x)
+        x = torch.flatten(x, start_dim=1)  # Flatten before passing to linear layer
+        x = self.fc_layers(x)
         return x
